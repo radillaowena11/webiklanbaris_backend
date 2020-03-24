8280a0a9b68ada22d61174bee4a6066e3e8d8fcb
radillaowena <radilla_nathaniel_27rpl@student.smktelkom-mlg.sch.id>
2020-03-24 20:37:10 +0700

    commit pertama

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['auth:api'])->get('/user', function (Request $request){
+
+    return $request->user();
+});
+
+Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@login');
+    Route::get('iklan', 'IklanController@Iklan');
+
+    Route::get('iklanall', 'IklanController@IklanAuth')->middleware('jwt.verify'); 
+    Route::get('user', 'UserController@getAuthenticatedUser')->middleware('jwt.verify'); 
